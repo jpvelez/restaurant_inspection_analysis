4029e68b46b9fcfc38e6c5ceda9be7c39341b268
Juan-Pablo Velez <jpvelez@gmail.com>
2012-07-11 17:21:32 -0500

    dumped data out of food_inspections.db (recent_restaurant_inspections.csv), normalized it in refine (recent_restaurant_inspections_normalized.csv) and imported it back into sqlite db using csvsql

--- data/shuffle_restaurants.py
+++ data/shuffle_restaurants.py
@@ -1,20 +1,15 @@
 import sqlite3
 import random
-# from YelpApi import YelpApi
 
 conn = sqlite3.connect('food_inspections.db')
 c = conn.cursor()
 
-c.execute("SELECT * FROM inspections_clean_restaurants_recent")
+c.execute("SELECT * FROM inspections_clean_restaurants_recent_normalized")
 
 recent_restaurant_inspections = c.fetchall()
 
 random.shuffle(recent_restaurant_inspections)
 
-for rest in recent_restaurant_inspections:
-    print '%s\t%s, Chicago, IL, %s' % (rest[2].title(), rest[6].title().strip(), rest[7])
-
-# api = YelpApi()
+# Print restaurants and addresses. Used to generate shuffled_restaurant_list.txt
 # for rest in recent_restaurant_inspections:
-#     full_address = rest[6].title() + ', Chicago, IL, ' + rest[7]
-#     api(full_address)
+#     print '%s\t%s, Chicago, IL, %s' % (rest[2].title(), rest[6].title().strip(), rest[7])
--- data/yelp_api/get_restaurants.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-NAME="restaurant"
-LOCATION='935 W Webster Ave, Chicago, IL, 60614'
-
-python search.py --consumer_key="88Ql36BiQPTsKqqUuIgIRA" --consumer_secret="d1Q1raKJppyMnDrRlTLj1cj2ygc" --token="vcge_k4r6cIFFPTt0AX1Elde6izhLtv0" --token_secret="5qFxwx4vmMU_86lI6ftxDdFo0Pc" --location="$LOCATION" --term="$NAME" --offset=0 --limit=20
