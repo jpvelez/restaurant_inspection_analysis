68d93ace4974aad7fce9a66e9493abe938f95a2c
Juan-Pablo Velez <jpvelez@gmail.com>
2012-07-30 15:15:28 -0500

    added findings pdf, got more data

--- munging/get_yelp_restaurants.py
+++ munging/get_yelp_restaurants.py
@@ -21,10 +21,10 @@ offset = ''
 limit = '10'
 host = 'api.yelp.com'
 
-json_output = open('..data/yelp_restaurants.json', 'w+')   # saves json response
+json_output = open('../data/yelp_restaurants.json', 'w+')   # saves json response
 
 # Try to fetch yelp data for each restaurant in recent inspection dataset
-for rest in inspections[1500:1700]:
+for rest in inspections[1900:2100]:
 
     # Setup restaurant name and location
     name = rest[2]
